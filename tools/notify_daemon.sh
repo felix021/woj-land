66983356a2bf910bfae69ec6b58bc4657e531067
felix021 <felix021@f4d92bd2-157e-11df-ae5d-f30dee00ba09>
2010-03-27 10:29:45 +0000

    增加notify daemon的小脚本

---
+++ tools/notify_daemon.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+host=localhost
+port=9527
+src_id=
+
+if [ -z $1 ]; then
+    echo Usage: $0 src_id [host [port]]
+    exit;
+fi
+src_id=$1
+
+if [ ! -z $2 ]; then
+    host=$2
+fi
+
+if [ ! -z $3 ]; then
+    port=$3
+fi
+
+echo -n "Returned: "
+echo $src_id | nc $host $port
+echo
