088b6eda41fab7e0d7a91d9eb0831cefa695df1d
felix021 <felix021@f4d92bd2-157e-11df-ae5d-f30dee00ba09>
2010-04-13 07:13:31 +0000

    增加题目提交统计页面

---
+++ code/web/module/status/problem/get.php
@@ -0,0 +1,89 @@
+<?php
+
+require_once(MODULE_ROOT . '/problem/problem.func.php');
+
+class Main extends cframe
+{
+
+    protected $need_session = true;
+    protected $need_login   = false;
+
+    public function process()
+    {
+        $page    = 1;
+        if (isset(request::$arr_get['page']))
+            $page    = (int)request::$arr_get['page'];
+        if ($page < 1)
+            $page = 1;
+
+        $itpp  = land_conf::STATUS_PER_PAGE;
+        $start = ($page - 1) * $itpp;
+
+        $pid     = (int)request::$arr_get['problem_id'];
+        $problem = get_problem_by_id($pid);
+        response::add_data('problem', $problem);
+
+        response::add_data('problem_id', $pid);
+        response::add_data('page', $page);
+
+        $conn = db_connect();
+        fail_test($conn, false);
+
+        $ac = land_conf::OJ_AC;
+        $sql = <<<eot
+SELECT `source_id`, `username`, `memory_usage`, `time_usage`, `lang`, `length`, `submit_time`
+    FROM `sources` 
+    WHERE `problem_id`=$pid AND `result`=$ac
+    ORDER BY `time_usage` ASC, `memory_usage` ASC
+    LIMIT $start,$itpp
+eot;
+        $lines = db_fetch_lines($conn, $sql, -1);
+        response::add_data('statuses', $lines);
+
+        $sql = 'SELECT `result`, COUNT(*) as `num` FROM `sources` GROUP BY `result`';
+        $lines = db_fetch_lines($conn, $sql, -1);
+        $results = array(
+            'AC'    => 0,
+            'PE'    => 0,
+            'TLE'    => 0,
+            'MLE'    => 0,
+            'WA'    => 0,
+            'RE'    => 0,
+            'OLE'    => 0,
+            'CE'    => 0,
+            'RF'    => 0,
+            'SE'    => 0,
+            'TOTAL'    => 0,
+            );
+        foreach ($lines as $line)
+        {
+            switch($line['result'])
+            {
+                case land_conf::OJ_WAIT: break;
+                case land_conf::OJ_AC: $results['AC'] += $line['num']; break;
+                case land_conf::OJ_PE: $results['PE'] += $line['num']; break;
+                case land_conf::OJ_TLE: $results['TLE'] += $line['num']; break;
+                case land_conf::OJ_MLE: $results['MLE'] += $line['num']; break;
+                case land_conf::OJ_WA: $results['WA'] += $line['num']; break;
+                case land_conf::OJ_OLE: $results['OLE'] += $line['num']; break;
+                case land_conf::OJ_CE: $results['CE'] += $line['num']; break;
+                case land_conf::OJ_RF: $results['RF'] += $line['num']; break;
+                case land_conf::OJ_SE: $results['SE'] += $line['num']; break;
+                default: $results['RE'] += $line['num']; break;
+            }
+            $results['TOTAL'] += $line['num'];
+        }
+        response::add_data('results', $results);
+
+        db_close($conn);
+        return true;
+    }
+
+    public function display()
+    {
+        $this->set_my_tpl("problem.tpl.php");
+        return true;
+    }
+}
+
+?>
---
+++ code/web/module/status/problem/problem.tpl.php
@@ -0,0 +1,100 @@
+<?php
+
+class TPL_Main extends ctemplate
+{
+    public function display($p)
+    {
+        $web_root = land_conf::$web_root;
+        $pro = $p['problem'];
+        $res = $p['results'];
+        $pid = (int)$p['problem_id'];
+        $page = $p['page'];
+        $prev = $page - 1;
+        $next = $page + 1;
+
+        echo <<<eot
+<style type="text/css"> 
+td{text-align:center;}
+</style> 
+<div id="tt"> 
+    Problem Status - <a href="$web_root/problem/detail?problem_id={$pid}" >{$pid}</a> 
+</div> 
+
+<div id="main"> 
+      <table><tbody> 
+      <tr> 
+        <th width="120">User Submitted</th> 
+        <th width="120">User Solved</th> 
+        <th width="80">Total</th> 
+        <th width="50">AC</th> 
+        <th width="50">PE</th> 
+        <th width="50">WA</th> 
+        <th width="50">TLE</th> 
+        <th width="50">RE</th> 
+        <th width="50">MLE</th> 
+        <th width="50">OLE</th> 
+        <th width="50">CE</th> 
+      </tr> 
+      <tr class="tro"> 
+        <td>{$pro['submitted']}</td> 
+        <td>{$pro['accepted']}</td> 
+        <td>{$res['TOTAL']}</td> 
+        <td>{$res['AC']}</td> 
+        <td>{$res['PE']}</td> 
+        <td>{$res['WA']}</td> 
+        <td>{$res['TLE']}</td> 
+        <td>{$res['RE']}</td> 
+        <td>{$res['MLE']}</td> 
+        <td>{$res['OLE']}</td> 
+        <td>{$res['CE']}</td> 
+      </tr> 
+      </tbody></table> 
+      <br/>
+
+    <table><tbody> 
+      <tr> 
+        <th width="60">Rank</th> 
+        <th width="70">Run ID</th> 
+        <th width="130">User</th> 
+        <th width="60">Time</th> 
+        <th width="70">Memory</th> 
+        <th width="90">Language</th> 
+        <th width="80">Length</th> 
+        <th width="170">Submit Time</th> 
+      </tr> 
+eot;
+        $i = 0;
+        if (is_array($p['statuses']))
+        foreach ($p['statuses'] as $status)
+        {
+            $tr_class = $i++ & 1 ? 'tro' : 'tre';
+            foreach ($status as &$v) $v = htmlspecialchars($v);
+            $lang = land_conf::$lang[$status['lang']];
+            echo <<<eot
+      <tr class="{$tr_class}"> 
+        <td>{$i}</td> 
+        <td>{$status['source_id']}</td> 
+        <td><a href="$web_root/user/detail?username={$status['username']}">{$status['username']}</a></td> 
+        <td>{$status['time_usage']}</td> 
+        <td>{$status['memory_usage']}</td> 
+        <td><a href="$web_root/source?source_id={$status['source_id']}">{$lang}</a></td> 
+        <td>{$status['length']}</td> 
+        <td>{$status['submit_time']}</td> 
+      </tr> 
+eot;
+        }
+        echo <<<eot
+    </tbody></table> 
+    <br/>
+    <span class="bt"><a href="$web_root/status/problem?problem_id={$pid}">Top</a></span> 
+    <span class="bt"><a href="$web_root/status/problem?page={$prev}&problem_id={$pid}">Prev Page</a></span> 
+    <span class="bt"><a href="$web_root/status/problem?page={$next}&problem_id={$pid}">Next Page</a></span>
+    <br/>
+    <br/>
+</div> 
+eot;
+        return true;
+    }
+}
+
+?>
