de12901ace3f0a5a0d062999ce5b3ab2ac37ded5
felix021 <felix021@f4d92bd2-157e-11df-ae5d-f30dee00ba09>
2010-04-20 05:42:14 +0000

    增加题目搜索

---
+++ code/web/module/problem/search/post.php
@@ -0,0 +1,56 @@
+<?php
+
+class Main extends cframe
+{
+    protected $need_login = false;
+
+    public function process()
+    {
+        $search = request::$arr_post['key'];
+        if (empty($search))
+        {
+            throw new Exception("Please provide the search field.");
+        }
+        $sql = <<<eot
+SELECT `problem_id`, `title`, `submitted`, `accepted`
+  FROM `problems`
+  WHERE `title` LIKE '%$search%'
+eot;
+        if (!is_admin())
+        {
+            $sql .= ' AND `enabled`=1';
+        }
+        $conn = db_connect();
+        fail_test($conn, false);
+        $problems = db_fetch_lines($conn, $sql, -1);
+        db_close($conn);
+        foreach ($problems as &$p)
+        {
+            $ac = $p['accepted'];
+            $st = $p['submitted'];
+            if ($st == 0)
+            {
+                $p['ratio'] = 0;
+            }
+            else
+            {
+                $p['ratio'] = round($ac * 100 / $st, 2);
+            }
+        }
+        response::add_data('problems', $problems);
+        response::add_data('volume', 'Found');
+        $solved_list = session::$is_login ? session::$user_info['solved_list'] : '';
+        response::add_data('solved', $solved_list);
+
+        return true;
+    }
+
+    public function display()
+    {
+        //$this->set_my_tpl("list.tpl.php");
+        response::set_tpl(MODULE_ROOT . '/problem/list/list.tpl.php');
+        return true;
+    }
+}
+
+?>
