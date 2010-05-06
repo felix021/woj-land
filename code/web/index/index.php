ce517dafb5e961028fee05e9fba563da986d4d41
felix021 <felix021@f4d92bd2-157e-11df-ae5d-f30dee00ba09>
2010-05-06 15:53:31 +0000

    在框架中增加记录exception的详细信息

--- code/web/index/index.php
+++ code/web/index/index.php
@@ -61,6 +61,12 @@ try
 }
 catch (Exception $e)
 {
+    if (logger::log_opened())
+    {   
+        FM_LOG_WARNING('Exception [%s:%d]: (%d)%s',
+            $e->getFile(), $e->getLine(), $e->getCode(), $e->getMessage());
+    }   
+
     response::display_err($e->getMessage());
 }
 
