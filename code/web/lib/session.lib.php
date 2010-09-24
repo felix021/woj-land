191fbb8e15926d3515e15cc92fc960e5bb18fc49
felix021 <felix021@f4d92bd2-157e-11df-ae5d-f30dee00ba09>
2010-09-24 10:03:50 +0000

    修改session的'域'，使session与其他(land)应用互不干扰

--- code/web/lib/session.lib.php
+++ code/web/lib/session.lib.php
@@ -12,12 +12,16 @@ final class session
     public static $priv      = null;
     public static $n_unread  = 0;
 
+    //使session独立于其他应用，不互相干扰
+    public static $sess_id   = null;
+
     public static function init($do_update = false)
     {
-        if (isset($_SESSION['land']['is_login']))
+        self::$sess_id = land_conf::$web_root;
+        if (isset($_SESSION[self::$sess_id]['is_login']))
         {
             self::$is_login  = true;
-            self::$user_id   = $_SESSION['land']['user_id'];
+            self::$user_id   = $_SESSION[self::$sess_id]['user_id'];
 
             if ($do_update)
             {
@@ -27,9 +31,9 @@ final class session
             }
             else
             {
-                self::$user_info = $_SESSION['land']['user_info'];
-                self::$priv      = $_SESSION['land']['priv'];
-                self::$n_unread  = $_SESSION['land']['n_unread'];
+                self::$user_info = $_SESSION[self::$sess_id]['user_info'];
+                self::$priv      = $_SESSION[self::$sess_id]['priv'];
+                self::$n_unread  = $_SESSION[self::$sess_id]['n_unread'];
             }
 
             logger::log_add_info("username:" . self::$user_info['username']);
@@ -45,10 +49,10 @@ final class session
         //匿名用户不允许登录
         if ($user_id != self::ANONYMOUS_ID)
         {
-            $_SESSION['land']['is_login']  = true;
+            $_SESSION[self::$sess_id]['is_login']  = true;
             self::$is_login             = true;
             
-            $_SESSION['land']['user_id']   = $user_id;
+            $_SESSION[self::$sess_id]['user_id']   = $user_id;
             self::$user_id              = $user_id;
         }
         else
@@ -68,7 +72,7 @@ final class session
     {
         if ($len <= 0) $len = 6;
         $vcode = rndstr($len);
-        $_SESSION['land']['vcode'] = $vcode;
+        $_SESSION[self::$sess_id]['vcode'] = $vcode;
         response::add_header('Pragma: no-cache');
         response::add_header('Cache-Control: no-cache, must-revalidate');
         response::add_header('Expires: 0');
@@ -78,10 +82,10 @@ final class session
     public static function get_vcode()
     {
         $vcode = false;
-        if (isset($_SESSION['land']['vcode']))
+        if (isset($_SESSION[self::$sess_id]['vcode']))
         {
-            $vcode = $_SESSION['land']['vcode'];
-            unset($_SESSION['land']['vcode']); //vcode只能用一次
+            $vcode = $_SESSION[self::$sess_id]['vcode'];
+            unset($_SESSION[self::$sess_id]['vcode']); //vcode只能用一次
         }
         return $vcode;
     }
@@ -110,7 +114,7 @@ final class session
 
         $user_info = null;
 
-        if ($do_update || !isset($_SESSION['land']['user_info']))
+        if ($do_update || !isset($_SESSION[self::$sess_id]['user_info']))
         {
 
             $conn = db_connect();
@@ -121,12 +125,12 @@ final class session
             db_close($conn);
 
             fail_test($user_info, false);
-            $_SESSION['land']['user_info'] = $user_info;
+            $_SESSION[self::$sess_id]['user_info'] = $user_info;
 
         }
         else
         {
-            $user_info = $_SESSION['land']['user_info'];
+            $user_info = $_SESSION[self::$sess_id]['user_info'];
         }
 
         self::$user_info = $user_info;
@@ -144,18 +148,18 @@ final class session
 
         $priv = null;
 
-        if ($do_update || !isset($_SESSION['land']['priv']))
+        if ($do_update || !isset($_SESSION[self::$sess_id]['priv']))
         {
             if (count(self::$user_info) == 0)
             {
                 self::$user_info = self::get_user_info($do_update);
             }
             $priv =  get_privileges_by_groups(self::$user_info['group_ids']);
-            $_SESSION['land']['priv'] = $priv;
+            $_SESSION[self::$sess_id]['priv'] = $priv;
         }
         else
         {
-            $priv = $_SESSION['land']['priv'];
+            $priv = $_SESSION[self::$sess_id]['priv'];
         }
 
         self::$priv = $priv;
@@ -173,7 +177,7 @@ final class session
 
         $n_unread = null;
 
-        if ($do_update || !isset($_SESSION['land']['n_unread']))
+        if ($do_update || !isset($_SESSION[self::$sess_id]['n_unread']))
         {
             $conn = db_connect();
             fail_test($conn, false);
@@ -185,11 +189,11 @@ final class session
             fail_test($n_unread, false);
             db_close($conn);
 
-            $_SESSION['land']['n_unread'] = $n_unread;
+            $_SESSION[self::$sess_id]['n_unread'] = $n_unread;
         }
         else
         {
-            $n_unread = $_SESSION['land']['n_unread'];
+            $n_unread = $_SESSION[self::$sess_id]['n_unread'];
         }
 
         self::$n_unread = $n_unread;
@@ -199,8 +203,8 @@ final class session
 
     public function read_mail()
     {
-        $_SESSION['land']['n_unread']--;
-        self::$n_unread = $_SESSION['land']['n_unread'];
+        $_SESSION[self::$sess_id]['n_unread']--;
+        self::$n_unread = $_SESSION[self::$sess_id]['n_unread'];
     }
 }
 
